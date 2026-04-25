e48fb7d50be9fde7cc965783862191e4a0f5e6bb
adhyayana108 <adhyayana108@gmail.com>
2026-04-25 10:31:44 +0530

    health and hello handler

--- web_server/main.go
+++ web_server/main.go
@@ -34,4 +34,42 @@ func loggingMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+// health check handler
+
+var serverStartTime = time.Now()
+
+func HealthHandler(w http.ResponseWriter , r *http.Request){
+	if r.Method != http.MethodGet {
+		http.Error(w , `{"error":"Method not allowed"}` , http.StatusMethodNotAllowed )
+		return
+	}
+
+	w.Header().Set("Content-Type" . "application/json")
+	uptime := time,since(serverStartTime).Round(time.second)
+
+	payload := map[string]string {
+		"status":    "OK",
+        "uptime":    uptime.String(),
+        "timestamp": time.Now().UTC().Format(time.RFC3339),
+        "version":   "1.0.0",
+	}
+
+	json.NewEncoder(w).Enocode(payload)
+}
+
+// hello handler 
+
+func helloHandler(w http.ResponseWriter , r *http.Request)
+{
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404-Page Not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodGet {
+		http.Error(w , "Method not supported" , http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello! The Go Web Server is running successfully.")
+}
 
